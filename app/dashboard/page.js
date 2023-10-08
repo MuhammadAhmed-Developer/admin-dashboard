2f57e1b090cc33a1875d66a70c458e94fdd13df5
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-10-08 21:08:29 +0500

    final

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,5 @@
-import React from 'react'
-import SideBar from '../(components)/SideBar'
 import Headercard from '../(components)/headercard'
-import TopBar from '../(components)/TopBar'
-import Layout from '../(components)/Layout'
+
 
 export default function Dashboard() {
   return (
